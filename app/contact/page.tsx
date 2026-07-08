273aa0c38e37b0d47391fb82ff61298182d9088d
burchdad <burchsl4@gmail.com>
2026-07-08 08:03:17 -0500

    Update Formspree contact endpoint

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -25,7 +25,7 @@ export default function Contact() {
     setSubmitError('');
 
     try {
-      const response = await fetch('https://formspree.io/f/mqenrqnj', {
+      const response = await fetch('https://formspree.io/f/xykqaaln', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
